e9b136fb47fbb85c7f0d775b69798bf9042354bf
Kazumasa Kojima <kojima@bugnitude.com>
2017-06-08 17:03:38 +0900

    Initial Commit

---
+++ SamplePDFViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SamplePDFViewer
+//
+//  Created by kojima on 2017/06/08.
+//  Copyright © 2017 bugnitude. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
